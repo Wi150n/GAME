60d09384b9724962ee49070aa2dd52e05847d8db
Wilson <wzhu7148@gmail.com>
2016-04-08 08:38:17 -0500

    Update SampleGame.java

--- Port/SampleGame.java
+++ Port/SampleGame.java
@@ -3,9 +3,17 @@
 // import some stuff
 
 public class SampleGame extends AndroidGame {
+  // This will load the loading screen
   @Override
   public Screen getInitScreen() {
     return new LoadingScreen(this);
+  }
+  
+  
+  // This is a method that will exit the application when it is loading; using the back button on Android
+  @Override
+  public void onBackPressed() {
+    getCurrentScreen().backButton();
   }
 }
 
