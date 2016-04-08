30b91331039de165183c404900e51572892ed76a
Wilson <wzhu7148@gmail.com>
2016-04-08 08:53:06 -0500

    Create LoadingScreen.java

---
+++ Port/LoadingScreen.java
@@ -0,0 +1,15 @@
+// some package here
+
+// import some stuff here
+
+public class LoadingSreen extends Screen {
+  public LoadingScreen(Game game) {
+    super(game);
+  }
+  
+  @Override
+  public void update(float deltaTime) {
+    Graphics g = game.getGraphics();
+  
+  @Override
+  public void render(float deltaTime) {}
