9435279d2a4957507e6f1a2be763e35a9cb19496
Wilson <wzhu7148@gmail.com>
2016-04-16 20:14:49 -0500

    Update Enemy.java

--- JukeMasters/core/src/com/cs3380/project/entity/Enemy.java
+++ JukeMasters/core/src/com/cs3380/project/entity/Enemy.java
@@ -2,6 +2,7 @@ package com.cs3380.project.entity;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
+import com.cs3380.project.TextureManager;
 
 public class Enemy extends Entity {
 
